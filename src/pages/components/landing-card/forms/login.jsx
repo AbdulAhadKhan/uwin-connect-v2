3333dd23ff33193e7734e3921af908c02cb56978
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-16 21:27:11 -0400

    Converted regular inputs to validated inputs in login

--- src/pages/components/landing-card/forms/login.jsx
+++ src/pages/components/landing-card/forms/login.jsx
@@ -1,11 +1,13 @@
+import ValidatedInput from "../../validated-fields/input"
+
 export default function Login() {
     return (
         <div className="landing-form">
             <h1>Login</h1>
             <form>
                 <hr />
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <ValidatedInput type="text" id="email" placeholder="UWindsor Email" blank={false} />
+                <ValidatedInput type="password" id="password" placeholder="Password" blank={false} />
                 <button type="submit">Log In</button>
                 <p>Don't have an account? <a href="#">Sign Up</a></p>
             </form>
