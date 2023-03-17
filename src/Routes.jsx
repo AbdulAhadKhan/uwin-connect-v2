5d2703e4c50839f1b4dc359c2af59dc8670260b8
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-16 23:40:32 -0400

    Added routes for login, signup, and 404

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,8 +2,15 @@ import { createBrowserRouter } from "react-router-dom"
 
 import Home from "./pages/Home"
 import Landing from "./pages/landing"
+import Login from "./pages/components/forms/login"
+import Signup from "./pages/components/forms/signup"
+import NotFound from "./pages/404"
 
 export const BrowserRouter = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/login", element: <Landing /> },
+    { path: "*", element: <NotFound /> },
+    { path: "/", element: <Landing />, children: [
+        { path: "/", element: <Login /> },
+        { path: "/signup", element: <Signup /> },
+    ] },
+    { path: "/home", element: <Home /> },
 ])
