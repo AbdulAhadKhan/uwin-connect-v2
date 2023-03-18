e00b29e7a93f4bbb5932d2042ebe8d1283be2f1f
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-18 03:29:14 -0400

    Added custom validation and conditional hinting

--- src/pages/components/validated-fields/input.jsx
+++ src/pages/components/validated-fields/input.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-function validateInput(input, setErrorState, blank) {
-    console.log(input);
-    if (!blank && input === "") {
-        setErrorState(" error");
+function validateInput(input, setErrorState, setValidity, customValidation, blank) {
+    customValidation = customValidation || ((input) => true)
+    
+    if (!blank && input === "" || !customValidation(input)) {
+        setErrorState(" error")
+        if (setValidity) setValidity(false)
     } else {
-        setErrorState("");
+        setErrorState("")
+        if (setValidity) setValidity(true)
     }
 }
 
-export default function ValidatedInput({type, id, placeholder, className="", blank}) {
-    const [errorState, setErrorState] = useState("");
+function onBlurEvent(event, setErrorState, setValidity, customValidation, blank, setParentState) {
+    validateInput(event.target.value, setErrorState, setValidity, customValidation, blank)
+    if (setParentState) setParentState(event.target.value)
+}
 
-    return <input type={type} id={id} placeholder={placeholder} 
-            className={className + errorState} onBlur={event => validateInput(event.target.value, setErrorState, blank)}/>
+export default function ValidatedInput({type, id, placeholder, className="", setIsInvalid,
+                                        hint, customValidation , blank=true, setParentState}) {
+    const [errorState, setErrorState] = useState("")
+    const validityWrapper = (validity) => setIsInvalid ? setIsInvalid(id, validity) : null
+
+    return (
+        <div className="validated-input">
+            <input type={type} id={id} placeholder={placeholder} className={ className + errorState} 
+             onBlur={event => onBlurEvent(event, setErrorState, validityWrapper, customValidation, blank, setParentState) }/>
+            {(hint !== undefined && errorState !== "") && <p className="hint">{hint}</p>}
+        </div>
+    )
 }
