358782effc588c94b15b43a8bb514dd8c827e53f
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-20 04:12:58 -0400

    Added a placeholder submit function

--- src/pages/components/forms/login.jsx
+++ src/pages/components/forms/login.jsx
@@ -2,11 +2,16 @@ import { NavLink } from "react-router-dom"
 
 import ValidatedInput from "../validated-fields/input"
 
+function onSubmit(event) {
+    event.preventDefault()
+    const form = FormData(event.target)
+}
+
 export default function Login() {
     return (
         <div className="landing-form">
             <h1>Login</h1>
-            <form>
+            <form onSubmit={(event) => onSubmit(event)} >
                 <hr />
                 <ValidatedInput type="text" id="email" placeholder="UWindsor Email" blank={false} />
                 <ValidatedInput type="password" id="password" placeholder="Password" blank={false} />
