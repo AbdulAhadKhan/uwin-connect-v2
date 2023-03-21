8f401ab554b1e37194f3d1887715b877f340ce8c
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-20 20:01:52 -0400

    Coverted html forms to Formik

--- src/pages/components/forms/login.jsx
+++ src/pages/components/forms/login.jsx
@@ -1,23 +1,54 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+import { Formik, Form, Field } from "formik"
 
-import ValidatedInput from "../validated-fields/input"
-
-function onSubmit(event) {
-    event.preventDefault()
-    const form = FormData(event.target)
+function onSubmit(values) {
+    console.log(values)
 }
 
 export default function Login() {
+    const [emailErrorClass, setEmailErrorClass] = useState("")
+    const [passwordErrorClass, setPasswordErrorClass] = useState("")
+    
+    const validateEmail = (email) => {
+        let error = ""
+        if (!email.endsWith("@uwindsor.ca") || email.length < 13) {
+            error = "UWindsor email is required"
+            setEmailErrorClass(" error")
+        }
+        else setEmailErrorClass("")
+        return error
+    }
+
+    const validatePassword = (password) => {
+        let error = ""
+        if (!password)
+            error = "Password is required"
+        else setPasswordErrorClass("")
+        return error
+    }
+
+    const fields = [
+        {name: "email", type: "email", placeholder: "Email", validation: validateEmail},
+        {name: "password", type: "password", placeholder: "Password", validation: validatePassword}
+    ]
+    
     return (
         <div className="landing-form">
             <h1>Login</h1>
-            <form onSubmit={(event) => onSubmit(event)} >
-                <hr />
-                <ValidatedInput type="text" id="email" placeholder="UWindsor Email" blank={false} />
-                <ValidatedInput type="password" id="password" placeholder="Password" blank={false} />
-                <button type="submit">Log In</button>
-                <p>Don't have an account? <NavLink to="/signup">Sign Up</NavLink></p>
-            </form>
+            <hr />
+            <Formik initialValues={{ email: "", password: "" }} onSubmit={onSubmit}>
+                {({ errors, touched }) => (
+                    <Form>
+                        <Field name="email" type="email" placeholder="Email" validate={validateEmail} />
+                        {errors.email && touched.email && <p className="hint">{errors.email}</p>}
+                        <Field name="password" type="password" placeholder="Password" validate={validatePassword} />
+                        {errors.password && touched.password && <p className="hint">{errors.password}</p>}
+                        <button type="submit">Login</button>
+                    </Form>
+                )}
+            </Formik>
+            <p>Don't have an account? <NavLink to="/signup">Sign Up</NavLink></p>
         </div>
     )
 }
