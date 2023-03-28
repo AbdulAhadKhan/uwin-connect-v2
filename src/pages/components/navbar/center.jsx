e86dc4a9180b492618d1b2ca3638347103b9ddb5
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-28 00:47:00 -0400

    Fixed searchbar's missing import

--- src/pages/components/navbar/center.jsx
+++ src/pages/components/navbar/center.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, createSearchParams } from 'react-router-dom'
 import { BiSearchAlt } from 'react-icons/bi'
 
 export default function Center({ value }) {
