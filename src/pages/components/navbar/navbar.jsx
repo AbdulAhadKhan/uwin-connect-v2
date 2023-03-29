9049855a94cdf28f3dc6255e1f23b738278a8b3e
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-28 21:01:32 -0400

    Change modal open state on new post click

--- src/pages/components/navbar/navbar.jsx
+++ src/pages/components/navbar/navbar.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
+
 import Left from './left'
 import Center from './center'
 import Right from './right'
+import NewPostModal from '../new-post/new-post'
 
 import './navbar.css'
 
 export default function Navbar({ hideCreate, value }) {
+    const [modalOpen, setModalOpen] = useState(false)
+
     return (
-        <nav className='navbar tripple-grid'>
-            <Left hideCreate={hideCreate} />
-            <Center value={value} />
-            <Right />
-        </nav>
+        <>
+            <nav className='navbar tripple-grid'>
+                <Left hideCreate={hideCreate} setModalOpen={setModalOpen} />
+                <Center value={value} />
+                <Right />
+            </nav>
+            {modalOpen && <NewPostModal setOpen={setModalOpen} />}
+        </>
     )
 }
