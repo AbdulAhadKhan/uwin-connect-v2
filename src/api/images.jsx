1f98ec5d9a602bafb3b3650f793da433a6940144
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-30 03:53:26 -0400

    Reformatting

--- src/api/images.jsx
+++ src/api/images.jsx
@@ -1,15 +1,17 @@
-import { instance } from "../utils"
+import { instance } from '../utils'
 
 export function getImage(image) {
-    return instance.get(`/get-image/${image}`, {headers: {
-        'Content-Type': 'image/*',
-    }})
+    return instance.get(`/get-image/${image}`, {
+        headers: {
+            'Content-Type': 'image/*',
+        },
+    })
 }
 
 export function uploadProfileImage(formData, email) {
     return instance.put(`/upload-profile-picture/${email}`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data',
-        }
+        },
     })
 }
